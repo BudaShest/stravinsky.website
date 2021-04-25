e8cefa44728b5285c7ce022bf1a470b98e3ffd62
BudaShest <budashest@gmail.com>
2021-04-25 22:35:16 +0500

    доделал адаптив на телефонах более менее

--- database/config.php
+++ database/config.php
@@ -1,9 +1,9 @@
 <?php
 const CONNECTION = [
     'host'=>'localhost',
-    'dbname'=>'u1363425_stravinsky',
-    'login'=>'u1363425_default',
-    'password'=>'u7I6pupkKS18xJ7Z',
+    'dbname'=>'stravinsky',
+    'login'=>'mysql',
+    'password'=>'mysql',
     'options'=>[
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
